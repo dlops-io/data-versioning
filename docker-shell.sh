8b5b98415794e62f077412913cbad8b154e78c6f
wyett <wyettzeng@gmail.com>
2025-11-08 19:18:58 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="wyett-ac215-hw2-data-versioning"
+export GCP_PROJECT="AC215"
+export GCP_ZONE="us-central1"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
